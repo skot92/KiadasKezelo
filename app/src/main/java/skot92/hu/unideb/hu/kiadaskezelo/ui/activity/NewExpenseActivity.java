c962efb7abbcc8bc634b3454080453f9a537ebc9
Tóth Dávid <skot9212@gmail.com>
2015-10-21 21:46:04 +0300

    kiadás...

--- app/src/main/java/skot92/hu/unideb/hu/kiadaskezelo/ui/activity/NewExpenseActivity.java
+++ app/src/main/java/skot92/hu/unideb/hu/kiadaskezelo/ui/activity/NewExpenseActivity.java
@@ -4,21 +4,54 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import skot92.hu.unideb.hu.kiadaskezelo.R;
+
 public class NewExpenseActivity extends ListActivity {
+
+    private Button btnNewItem;
+    private Button btnSaveExpense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense);
+
+
         String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
                 "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
                 "Linux", "OS/2" };
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
+
+        btnNewItem = (Button) findViewById(R.id.btnAddNewItem);
+        btnSaveExpense = (Button) findViewById(R.id.btnSaveExpense);
+
+        controll();
     }
+
+    public void controll() {
+        btnNewItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnSaveExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
+
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
